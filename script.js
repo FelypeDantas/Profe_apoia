144f2d6cd3070e97d3580268846011c56be99c0b
Felype <felyped03@gmail.com>
2026-06-11 11:17:12 -0300

    Implement image modal for feedback section
    Added modal functionality for image feedback display.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const imagens = document.querySelectorAll(".feedbacks img");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+
+imagens.forEach(img=>{
+    img.addEventListener("click",()=>{
+        modal.style.display="flex";
+        modalImg.src = img.src;
+    });
+});
+
+modal.addEventListener("click",()=>{
+    modal.style.display="none";
+});
+
 function openTab(tabId) {
   const contents = document.querySelectorAll(".tab-content");
   const tabs = document.querySelectorAll(".tab");
